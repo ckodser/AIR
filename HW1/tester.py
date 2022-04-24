d238f018033c1bfc917adb3874c46701e6baa96a
ckodser <arshia.soltani2@gmail.com>
2022-04-25 01:15:16 +0430

    add wiki test

--- HW1/tester.py
+++ HW1/tester.py
@@ -18,17 +18,20 @@ with open("tests.txt", encoding="utf-8") as f:
             print('*' * 10)
         print()
 
-# with open("bigtest.txt", encoding="utf-8") as f:
-#     total = ""
-#     for input in f:
-#         if len(input) < 1:
-#             continue
-#         total += input + "\n"
-#
-#     input=total
-#     print(input)
-#     for qu in run(input):
-#         print("question:", normalizer.normalize(qu["Question"]), "ans:", normalizer.normalize(qu["Answer"]))
-#     print()
+with open("bigtest.txt", encoding="utf-8") as f:
+    total = ""
+    for input in f:
+        if len(input) < 1:
+            continue
+        total += input + "\n"
+
+    input=total
+    print(input, " ==> ")
+    for qu in run(input):
+        print('question:', normalizer.normalize(qu["Question"]))
+        print('ans:', normalizer.normalize(qu["Answer"]))
+        print('fn_name:', qu['fn_name'])
+        print('*' * 10)
+    print()
 
 
