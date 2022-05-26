c4f8268ff6712f90a28cb77bde82044545a0bcbd
ckodser <arshia.soltani2@gmail.com>
2022-05-26 09:49:10 +0430

    WIKI cookbook

--- HW2/crawler/tutorial/tutorial/spiders/quotes_spider.py
+++ HW2/crawler/tutorial/tutorial/spiders/quotes_spider.py
@@ -4,64 +4,35 @@ import re
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "iranplit"
+    name = "WIKIcookbook"
 
     start_urls = [
-        'https://fa.wikipedia.org/wiki/%D8%B1%D8%AF%D9%87:%D8%B3%DB%8C%D8%A7%D8%B3%D8%AA_%D8%AF%D8%B1_%D8%A7%DB%8C%D8%B1%D8%A7%D9%86'
+        'https://fa.wikibooks.org/wiki/%DA%A9%D8%AA%D8%A7%D8%A8_%D8%A2%D8%B4%D9%BE%D8%B2%DB%8C/%D9%81%D9%87%D8%B1%D8%B3%D8%AA_%D8%BA%D8%B0%D8%A7%D9%87%D8%A7%DB%8C_%D9%85%D8%AD%D9%84%DB%8C_%D8%A7%DB%8C%D8%B1%D8%A7%D9%86'
     ]
-    dict_rade = []
-    dict_wiki = []
 
     def parse(self, response):
-        QuotesSpider.dict_rade.append(response.url)
-        rades = []
-        for link_sec in response.css('.CategoryTreeItem'):
-            rade = response.urljoin(link_sec.css("a::attr(href)").get())
-            rades.append(rade)
         wikis = []
-
-        for link_sec in response.css('.mw-category-group'):
-            link_sec = link_sec.css("ul")
+        for link_sec in response.css('.mw-parser-output'):
+            link_sec = link_sec.css("ol")
             for links in link_sec.css("li"):
                 wiki = response.urljoin(links.css("a::attr(href)").get())
                 wikis.append(wiki)
 
-        wikis = [x for x in wikis if x not in rades]
-        print(f"in rade-{len(QuotesSpider.dict_rade)} , inside rade:{len(rades)}, inside wiki:{len(wikis)}")
-        for wiki in wikis:
-            yield scrapy.Request(wiki, callback=self.parser_wiki_page)
-
-        for rade in rades:
-            yield scrapy.Request(rade, callback=self.rade_parse)
-
-    def rade_parse(self, response):
-        QuotesSpider.dict_rade.append(response.url)
-        # print("rade:",len(QuotesSpider.dict_rade))
-        rades = []
-        for link_sec in response.css('.CategoryTreeItem'):
-            rade = response.urljoin(link_sec.css("a::attr(href)").get())
-            rades.append(rade)
-        wikis = []
-
-        for link_sec in response.css('.mw-category-group'):
-            link_sec = link_sec.css("ul")
-            for links in link_sec.css("li"):
-                wiki = response.urljoin(links.css("a::attr(href)").get())
-                wikis.append(wiki)
-
-        wikis = [x for x in wikis if x not in rades]
-        print(f"in subrade-{len(QuotesSpider.dict_rade)} , inside rade:{len(rades)}, inside wiki:{len(wikis)}")
-
-        for wiki in wikis:
+        for step, wiki in enumerate(wikis):
             yield scrapy.Request(wiki, callback=self.parser_wiki_page)
 
     def parser_wiki_page(self, response):
-        if len(QuotesSpider.dict_wiki) > 6 and random.random() > 0.22:
-            return
-        QuotesSpider.dict_wiki.append(response.url)
-        print("wiki:", len(QuotesSpider.dict_wiki))
-
+        name = response.xpath('//h1[@id="firstHeading"]/text()').get()
+        name = re.sub("کتاب آشپزی/", "", name)
+        print("wiki:", name)
+        js = {"name": name, "url": response.url}
+        text = ""
         for step, e in enumerate(response.css('div#mw-content-text>div>p')):
             para = e.get()
-            # para= re.sub("[a-zA-Z><%\"#]+", "",para)
-            yield {'para': para}
+            text += para
+        js["Preparation"] = text
+        js["ingredients"] = []
+        for step, e in enumerate(response.css('div#mw-content-text>div>ol>li')):
+            para = e.get()
+            js["ingredients"].append(para)
+        yield js
